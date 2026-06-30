b39d3003d93977a661e985920e6b54ff40a5e4e0
AdrianDev0816 <adriano08bus@gmail.com>
2026-06-29 20:06:52 -0600

    fix: agregar promart-web.vercel.app al CORS

--- backend-promart/src/main/java/com/inventario/backend/config/CordsConfig.java
+++ backend-promart/src/main/java/com/inventario/backend/config/CordsConfig.java
@@ -13,7 +13,7 @@ public class CordsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:4200" , "http://localhost" , "http://localhost:80"
+                "http://localhost:4200" , "http://localhost" , "http://localhost:80" , "https://promart-web.vercel.app"
          ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
